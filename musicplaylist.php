af277ea5846294feaf0d6bc0ecaf1932355884dd
Jasper2002 <78854014+Woofium@users.noreply.github.com>
2021-11-16 09:40:44 +0100

    musicpage added
    .

---
+++ musicplaylist.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  </head>
+        <body>
+        <div class="container p-2 text-center">
+        <h3>Music Playlist</h3>
+      </div>
+        <div class="container border-top border-dark  ">
+        <form class="border-bottom border-dark p-2">
+        <div class="d-none d-sm-block">
+            <blockquote class="blockquote mb-1">
+            <div class="row mt-3">
+            <div class="card order-1" style="width: 13rem;">
+            <img class="card-img-top" src="./musicimg/232x232-2015.png" alt="Card image cap">
+            <div class="">
+                <h5 class="card-title">Top hits of 2015</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+            </ul>
+            <div class="card-body text-center">
+            <a class="btn btn-success btn-block rounded-pill" href="https://open.spotify.com/playlist/37i9dQZF1DX9ukdrXQLJGZ" role="button">Spotify</a>
+            </div>
+        </div>
+            <div class="card" style="width: 13rem;">
+            <img class="card-img-top" src="./musicimg/232x232-2013.png" alt="Card image cap">
+            <div class="">
+                <h5 class="card-title">Top hits of 2013</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+            </ul>
+            <div class="card-body text-center">
+            <a class="btn btn-success btn-block rounded-pill" href="https://open.spotify.com/playlist/37i9dQZF1DX3Sp0P28SIer" role="button">Spotify</a>
+            </div>
+            </div>
+            <div class="card" style="width: 13rem;">
+            <img class="card-img-top" src="./musicimg/232x232-2014.png" alt="Card image cap">
+            <div class="">
+                <h5 class="card-title">Top hits of 2014</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+            </ul>
+            <div class="card-body text-center">
+            <a class="btn btn-success btn-block rounded-pill" href="https://open.spotify.com/playlist/37i9dQZF1DX0h0QnLkMBl4" role="button">Spotify</a>
+            </div>
+        </div>
+        <div class="card order-2" style="width: 13rem;">
+            <img class="card-img-top" src="./musicimg/232x232-2016.png" alt="Card image cap">
+            <div class="">
+                <h5 class="card-title">Top hits of 2016</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+            </ul>
+            <div class="card-body text-center">
+            <a class="btn btn-success btn-block rounded-pill" href="https://open.spotify.com/playlist/37i9dQZF1DX8XZ6AUo9R4R" role="button">Spotify</a>
+            </div>
+        </div>
+            <div class="card order-3" style="width: 13rem;">
+            <img class="card-img-top" src="./musicimg/232x232-2017.png" alt="Card image cap">
+            <div class="">
+                <h5 class="card-title">Top hits of 2017</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+            </ul>
+            <div class="card-body text-center">
+            <a class="btn btn-success btn-block rounded-pill" href="https://open.spotify.com/playlist/37i9dQZF1DWTE7dVUebpUW" role="button">Spotify</a>
+            </div>
+            </div>
+            <div class="card order-10" style="width: 13rem;">
+            <img class="card-img-top" src="./musicimg/longestmixtape_232x232.png" alt="Card image cap">
+            <div class="">
+                <h5 class="card-title">Mixtape</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+            </ul>
+            <div class="card-body text-center">
+            <a class="btn btn-success btn-block rounded-pill" href="https://open.spotify.com/playlist/4Dg0J0ICj9kKTGDyFu0Cv4" role="button">Spotify</a>
+            </div>
+        </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
